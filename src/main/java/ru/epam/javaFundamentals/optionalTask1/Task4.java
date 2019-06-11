2f0f660bb2f6e097c9a6bc9b54281c22fbf5846b
Olga Pyshkina <ospyshkina@yandex.ru>
2019-06-11 16:46:09 +0300

    Task 4

---
+++ src/main/java/ru/epam/javaFundamentals/optionalTask1/Task4.java
@@ -0,0 +1,15 @@
+package ru.epam.javaFundamentals.optionalTask1;
+
+import java.util.Scanner;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int a = scanner.nextInt();
+		System.out.println("Enter second number: ");
+		int b = scanner.nextInt();
+		System.out.println("a+b = " + (a + b) + "\na*b = " + a * b);
+	}
+}
