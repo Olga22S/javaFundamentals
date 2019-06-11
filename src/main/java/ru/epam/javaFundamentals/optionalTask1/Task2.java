0d804442d5801768e4181b9f5cb6ca70ab778ce6
Olga Pyshkina <ospyshkina@yandex.ru>
2019-06-11 07:21:03 +0300

    Task 2 was finished

--- src/main/java/ru/epam/javaFundamentals/optionalTask1/Task2.java
+++ src/main/java/ru/epam/javaFundamentals/optionalTask1/Task2.java
@@ -1,10 +1,13 @@
 package ru.epam.javaFundamentals.optionalTask1;
 
+import java.util.Scanner;
+
 public class Task2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type symbols:");
+		String symbols = scanner.next();
+		System.out.println(new StringBuffer(symbols).reverse().toString());
 	}
-
 }
