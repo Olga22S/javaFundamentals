620f05ba55b48cd3ee5de3355de870193430e9f8
Olga Pyshkina <ospyshkina@yandex.ru>
2019-06-19 02:35:59 +0300

    OptionalTask3

---
+++ src/main/java/optionalTask3/Task1.java
@@ -0,0 +1,18 @@
+package optionalTask3;
+
+import java.time.Month;
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of Month: ");
+		int userInput = scanner.nextInt();
+		if (userInput >= 1 && userInput <= 12) {
+			System.out.println(Month.of(userInput));
+		} else {
+			System.out.println("Incorrect value!");
+		}
+	}
+}
