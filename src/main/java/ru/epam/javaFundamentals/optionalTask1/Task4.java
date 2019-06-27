b3c7b90aa6ed3a9027d231c6adb918ec16fb42fd
Olga Pyshkina <ospyshkina@yandex.ru>
2019-06-27 17:34:23 +0300

    rename package

--- src/main/java/ru/epam/javaFundamentals/optionalTask1/Task4.java
+++ src/main/java/ru/epam/javaFundamentals/optionalTask1/Task4.java
@@ -3,7 +3,7 @@ package ru.epam.javaFundamentals.optionalTask1;
 import java.util.Scanner;
 
 public class Task4 {
-
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter first number: ");
